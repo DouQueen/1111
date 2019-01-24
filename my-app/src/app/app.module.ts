b548d45df49acb5ce65583b41f71f7860cf683f4
douxingli <13550938052@163.com>
2019-01-24 18:41:54 +0800

    '添加组件'

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ my-app/src/app/hero.ts
@@ -0,0 +1,5 @@
+export class Hero {
+   name: string;
+   id: number;
+
+}
