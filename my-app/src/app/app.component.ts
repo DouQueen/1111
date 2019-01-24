3bb7d0b00ecfd1d12a626ebc29af01d5e128f362
douxingli <13550938052@163.com>
2019-01-24 18:23:17 +0800

    '初始化文件'

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '水电费华盛顿和规范会计师的华国锋会计师的反馈国际化单位';
+}
